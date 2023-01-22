10de5210c484f8b9ad53a24fc0e6a72c86d279ae
michelange2008 <michelange@wanadoo.fr>
2023-01-22 17:49:56 +0100

    mise à jour de la présentation et synchro des images

--- routes/web.php
+++ routes/web.php
@@ -46,12 +46,12 @@ Route::get('/presentation', [VisiteurController::class, 'presentation'])->name('
 //     return view('dashboard');
 // })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+// Route::middleware('auth')->group(function () {
 
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    // Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    // Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     // Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+// });
 
 Route::middleware('auth', 'verified')->group(function () {
   Route::prefix('/accueil')->controller(AccueilController::class)->group(function() {
