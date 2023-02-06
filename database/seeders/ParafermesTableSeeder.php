a9ab4017efb49e3864fe4590f3b0451d2676cba0
michelange2008 <michelange@wanadoo.fr>
2023-02-06 17:20:01 +0100

    création de csv et des seeders utilisant les csv: chiffres, alertes, origines, critalertes, numalertes, parafermes.

--- database/seeders/ParafermesTableSeeder.php
+++ database/seeders/ParafermesTableSeeder.php
@@ -2,65 +2,35 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Seeder;
+use Flynsarmy\CsvSeeder\CsvSeeder;
 
-class ParafermesTableSeeder extends Seeder
+class ParafermesTableSeeder extends CsvSeeder
 {
+  public function __construct()
+  {
+      $this->table= 'parafermes';
+      $this->csv_delimiter = ';';
+      $this->filename = base_path('storage/csv/pb_parafermes.csv');
+      $this->should_trim = true;
+  }
 
-    /**
-     * Auto generated seed file
-     *
-     * @return void
-     */
-    public function run()
-    {
-        
+  /**
+   * Auto generated seed file
+   *
+   * @return void
+   */
+  public function run()
+  {
+    \DB::disableQueryLog();
 
-        \DB::table('parafermes')->delete();
-        
-        \DB::table('parafermes')->insert(array (
-            0 => 
-            array (
-                'id' => 3,
-                'nom' => 'SAU',
-                'type' => 'int',
-                'unite' => 'ha',
-                'parties' => NULL,
-                'ordre' => 2,
-                'supprimable' => 1,
-            ),
-            1 => 
-            array (
-                'id' => 24,
-                'nom' => 'Alimentation',
-                'type' => 'liste',
-                'unite' => NULL,
-                'parties' => 'maïs-soja, tout herbe, foin pâturage, choucroute',
-                'ordre' => 3,
-                'supprimable' => 1,
-            ),
-            2 => 
-            array (
-                'id' => 25,
-                'nom' => 'UTH',
-                'type' => 'float',
-                'unite' => NULL,
-                'parties' => NULL,
-                'ordre' => 1,
-                'supprimable' => 1,
-            ),
-            3 => 
-            array (
-                'id' => 26,
-                'nom' => 'Logement',
-                'type' => 'liste',
-                'unite' => NULL,
-                'parties' => 'aire paillée, logettes, plein air',
-                'ordre' => NULL,
-                'supprimable' => 1,
-            ),
-        ));
-        
-        
-    }
+    \DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+
+    \DB::table($this->table)->truncate();
+
+    \DB::statement('SET FOREIGN_KEY_CHECKS = 1');
+
+    parent::run();
+
+  }
+
 }
