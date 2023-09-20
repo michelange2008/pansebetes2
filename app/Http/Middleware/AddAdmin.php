5e4f6db8446762be04be652f9362dd546914d5c0
michelange2008 <michelange@wanadoo.fr>
2023-09-20 06:58:46 +0200

    modification middleware AdAdmin

--- app/Http/Middleware/AddAdmin.php
+++ app/Http/Middleware/AddAdmin.php
@@ -6,6 +6,12 @@ use Closure;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
+/**
+ * Ajoute la propriété isAdmin à l'user connecté 
+ * Cette propriété à la valeur true si celui-ci a un role_id  = 1 (webmaster) ou 2 (administrateur du site)
+ * et false dans les autres cas
+ * Cela permet l'affichage d'un menu plus complet que pour les autres utilisateurs.
+ */
 class AddAdmin
 {
     /**
@@ -19,7 +25,7 @@ class AddAdmin
     {
       if(Auth::user() != null) {
 
-        Auth::user()->isAdmin = (Auth::user()->role_id == 3) ? false : true;
+        Auth::user()->isAdmin = (Auth::user()->role_id == 1 || Auth::user()->role_id == 2) ? true : false;
 
       }
         return $next($request);
--- app/Http/Middleware/IsAdmin.php
+++ app/Http/Middleware/IsAdmin.php
@@ -23,5 +23,6 @@ class IsAdmin
         return $next($request);
       }
       return new RedirectResponse(url('/'));
+      
     }
 }
