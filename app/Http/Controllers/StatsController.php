1044374b96777f06ba0b9a315f6dd58b1f092b27
Michel BOUY <michelange@wanadoo.fr>
2023-09-24 19:30:40 +0200

    Mise à jour des graphiques

--- app/Http/Controllers/StatsController.php
+++ app/Http/Controllers/StatsController.php
@@ -7,23 +7,63 @@ use App\Models\Saisie;
 use App\Models\User;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
+use LaravelDaily\LaravelCharts\Classes\LaravelChart;
 
 /**
  * Affichage des statistiques d'utilisation du site
  */
 class StatsController extends Controller
 {
-    function index() : View {
+    function index(): View
+    {
 
-        $nb_utilisateurs = User::where('role_id', 1)->count();
+        $nb_utilisateurs = User::where('role_id', '<>', 1)->count();
+        $nb_user_saisie = Saisie::all()->groupBy('user_id')->count();
         $nb_saisies = Saisie::count();
 
+        $nb_pb_mensuel_options = [
+            'chart_title' => 'Nombre mensuel de Panses-bêtes',
+            'report_type' => 'group_by_date',
+            'model' => 'App\Models\Saisie',
+            'group_by_field' => 'created_at',
+            'group_by_period' => 'month',
+            'chart_type' => 'bar',
+        ];
+
+        $nb_pb_mensuel = new LaravelChart($nb_pb_mensuel_options);
+
+        $origine_users = [
+            'chart_title' => 'Origine des utilisateurs.trices',
+            'report_type' => 'group_by_relationship',
+            'model' => 'App\Models\User',
+            'relationship_name' => "region",
+            'group_by_field' => 'nom',
+            'chart_type' => 'pie',
+        ];
+
+        $origine_users = new LaravelChart($origine_users);
+
+        $profession_users = [
+            'chart_title' => 'Professions des utilisateurs.trices',
+            'report_type' => 'group_by_relationship',
+            'model' => 'App\Models\User',
+            'relationship_name' => "profession",
+            'group_by_field' => 'nom',
+            'chart_type' => 'pie',
+        ];
+
+        $profession_users = new LaravelChart($profession_users);
+
         $titre = new Titre("stats_clair.svg", "Statistiques d'utilisations de Panse-Bête", false);
-        
+
         return view('stats.index', [
             'nb_utilisateurs' => $nb_utilisateurs,
+            'nb_user_saisie' => $nb_user_saisie,
             'nb_saisies' => $nb_saisies,
             'titre' => $titre,
+            'nb_pb_mensuel' => $nb_pb_mensuel,
+            'origine_users' => $origine_users,
+            'profession_users' => $profession_users,
         ]);
     }
 }
--- resources/views/stats/index.blade.php
+++ resources/views/stats/index.blade.php
@@ -25,9 +25,8 @@
             </div>
 
         </div>
-        
-        <div class="mt-3 row justify-content-center">
 
+        <div class="my-3 row justify-content-center">
 
             <div class="col-sm-11 col-md-10 col-lg-9">
 
@@ -35,22 +34,23 @@
 
                     <div class="card">
                         <div class="text-center card-body">
-                            <h5>Nombre d'utilisateurs</h5>
-                            <h3>{{ $nb_utilisateurs }}</h3>
+                            <h3>{{ $nb_utilisateurs }} utilisateurs</h3>
+                            <h5>enregistrés</h5>
                         </div>
                     </div>
-                    
+
                     <div class="card">
                         <div class="text-center card-body">
-                            <h5>Nombre de saisies</h5>
-                            <h3>{{ $nb_saisies }}</h3>
+                            <h3>{{ $nb_saisies }} panse-bêtes</h3>
+                            <h5>réalisés par {{ $nb_user_saisie }} utilisateurs</h5>
+
                         </div>
                     </div>
-                    
+
                     <div class="card">
                         <div class="text-center card-body">
-                            <h5>Moyenne par utilisateur</h5>
-                            <h3>{{ $nb_saisies/$nb_utilisateurs }}</h3>
+                            <h3>{{ round($nb_saisies / $nb_user_saisie, 1) }} panse-bêtes</h3>
+                            <h5>réalisés en moyenne par utilisateur</h5>
                         </div>
                     </div>
 
@@ -60,5 +60,35 @@
 
         </div>
 
+        <div class="my-5 row justify-content-center">
+
+            <div class="col-sm-11 col-md-10 col-lg-9">
+                <h2>Evolution du nombre de saisies</h2>
+                {{ $nb_pb_mensuel->renderHtml() }}
+
+            </div>
+        </div>
+
+        <div class="my-3 row justify-content-center">
+
+            <div class="col-sm-6 col-md-5 col-lg-4">
+                <h2>Origine des utilisateurs.trices</h2>
+                {{ $origine_users->renderHtml() }}
+
+            </div>
+            <div class="col-sm-6 col-md-5 col-lg-4">
+                <h2>Profession des utilisateurs.trices</h2>
+                {{ $profession_users->renderHtml() }}
+
+            </div>
+        </div>
+
     </div>
+@endsection
+
+@section('scripts')
+    {!! $nb_pb_mensuel->renderChartJsLibrary() !!}
+    {!! $nb_pb_mensuel->renderJs() !!}
+    {!! $origine_users->renderJs() !!}
+    {!! $profession_users->renderJs() !!}
 @endsection
